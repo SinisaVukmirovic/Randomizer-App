4ba9a359ddaef4ac3ad0c2d923d119e8a5319afd
Sinisa Vukmirovic <sinisavukmirovic@gmail.com>
2021-10-18 21:28:37 +0200

    Trying functionality on test branch

--- js/app.js
+++ js/app.js
@@ -1,1 +1,20 @@
-console.log('sss');
+const inputElem = document.querySelector('#inputElem');
+const addBtn = document.querySelector('#addBtn');
+
+const items = [];
+
+const logInp = (e) => {
+    e.preventDefault();
+    // console.log(inputElem.value);
+    items.push(inputElem.value);
+    inputElem.value = '';
+
+    logUnique(items);
+}
+
+const logUnique = (items) => {
+    const uniqueItems = new Set(items);
+    console.log(uniqueItems);
+}
+
+addBtn.addEventListener('click', logInp);
